6aa0557e00ce0f4f3337a96270fc93fdf05062d1
Stephanie Orpilla <18673328+stephjs@users.noreply.github.com>
2021-01-10 17:52:40 -0800

    quick cron job. send fun messages every monday morning

---
+++ README.md
@@ -0,0 +1,12 @@
+### Cron Job for SMS HMU (Sunday scaries Messaging Service to Hype Mondays Up! 🎉)
+
+Check out my SMS HMU API Repo: https://github.com/stephjs/sms-hype-mondays-up
+
+Cron Job deployed on [Digital Ocean](https://cloud.digitalocean.com/): Every Monday at 07:30 am send a funny / motivational message (GET request to my SMS HMU API) to all my friends and me.
+
+I used [crontab guru](https://crontab.guru/#30_7_*_*_1] to figure out the correct expression for my desired frequency.
+
+Todo
+
+- move phone numbers out of .env and into a db
+- add a CMS for messages
---
+++ index.js
@@ -0,0 +1,46 @@
+const cron = require('node-cron');
+const express = require('express');
+const https = require('https');
+const escapeStringRegexp = require('escape-string-regexp');
+require('dotenv').config();
+
+app = express();
+const port = process.env.PORT || 3000;
+
+// todo db for messages / numbers. for now using dotenv so I don't leak my phone number
+const api = process.env.SMS_API;
+const message = process.env.SECRET_MONDAY_MESSAGE;
+const numbers = process.env.FRIENDS_PHONE_NUMBERS.split(' ');
+
+const sendMessageToNumbers = (phoneNumbers, message) => {
+  phoneNumbers.forEach((number) => {
+    const escapedMessage = escapeStringRegexp(message).split(' ').join('%20');
+    const options = {
+      hostname: api,
+      path: '/?message=' + escapedMessage + '&number=' + number,
+      method: 'GET',
+    };
+
+    const req = https.request(options, (res) => {
+      console.log(`statusCode: ${res.statusCode}`);
+
+      res.on('data', (d) => {
+        process.stdout.write(d);
+      });
+    });
+
+    req.on('error', (error) => {
+      console.error(error);
+    });
+
+    req.end();
+  });
+};
+
+// Every Monday at 07:30 am send a funny message to the list of numbers
+const cronJobFrequency = '30 7 * * 1';
+cron.schedule(cronJobFrequency, function () {
+  sendMessageToNumbers(numbers, message);
+});
+
+app.listen(port);
